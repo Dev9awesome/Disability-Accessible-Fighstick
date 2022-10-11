4e54d2a8ee5a6388c5e1af49f435bdc00a69d09b
Dev Patel <dev.awesome9@gmail.com>
2022-10-11 00:22:12 -0500

    Created methods to add keystroke functionality
    Arduinos are very simple
    Keyboard Library isn't being recognized, however

---
+++ src/KeyMapping.ino
@@ -0,0 +1,32 @@
+#include <Keyboard.h>
+
+#include <Keyboard.h>
+
+
+int pins[] = {0, 1, 2};
+char keys[] = {'a', 'b', 'c'};
+
+void setup() {
+
+  Keyboard.begin()
+ 
+}
+
+void addKey(int pin, char key){
+  
+  while(digitalRead(pin) == HIGH){
+    Keyboard.press(key);
+    }
+  Keyboard.release(key);
+  }
+
+
+
+void loop() {
+  for (int i = 0; i < sizeof(pins); i++){
+    
+    addKey(pins[i], keys[i]);
+    
+    }
+
+}
